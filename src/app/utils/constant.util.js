4f62e81f7529bb2b8eeec14b281b8e9da2b34b1f
tglima <thg.limadesousa@gmail.com>
2023-10-18 20:33:06 -0300

    feat: Adiciona novas mensagens

--- src/app/utils/constant.util.js
+++ src/app/utils/constant.util.js
@@ -2,6 +2,11 @@ let instance;
 
 class ConstantUtil {
   constructor() {
+    process.env.SWAGGER_URL = process.env.SWAGGER_URL.replace(
+      '{{NU_PORT}}',
+      process.env.NU_PORT
+    );
+
     this.MsgStatus400 =
       'Ocorreu uma falha/erro na sua requisição. Reveja os dados enviados e tente novamente!';
     this.MsgStatus401 = 'Credenciais inválidas ou ausentes';
@@ -9,6 +14,8 @@ class ConstantUtil {
     this.MsgStatus429 =
       'Limite de requisições ultrapassado, por favor, aguarde.';
     this.MsgStatus500 = 'Erro interno no servidor!';
+    this.MsgInvalidID = 'id não informado ou inválido!';
+    this.MsgStartAPI = `API listening on ${process.env.NU_PORT}\nSwagger link: ${process.env.SWAGGER_URL}`;
   }
 
   static getInstance() {
