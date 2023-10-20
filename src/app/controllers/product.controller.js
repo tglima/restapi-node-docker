3be638663689337023278feb3ea6113dcc67f125
tglima <thg.limadesousa@gmail.com>
2023-10-20 19:53:18 -0300

    refact: Alterada para utilizar a ProductRepository

--- src/app/controllers/product.controller.js
+++ src/app/controllers/product.controller.js
@@ -1,74 +1,44 @@
+import productRepository from '../repository/sqlite/product.repository';
 import constantUtil from '../utils/constant.util';
 
-const jsonProducts = {
-  products: [
-    {
-      id_product: 1,
-      nm_product: 'Basic',
-      vl_month_price: 14.99,
-      nm_videoQuality: 'Good',
-      nm_resolution: '480p',
-      qt_simultaneous_screens: 1,
-    },
-    {
-      id_product: 2,
-      nm_product: 'Standard',
-      vl_month_price: 34.99,
-      nm_videoQuality: 'Better',
-      nm_resolution: '1080p',
-      qt_simultaneous_screens: 2,
-    },
-    {
-      id_product: 3,
-      nm_product: 'Premium',
-      vl_month_price: 54.99,
-      nm_videoQuality: 'Best',
-      nm_resolution: '4K+HDR',
-      qt_simultaneous_screens: 4,
-    },
-  ],
-};
-
-function findProductById(id_product) {
-  const returnDTO = { status: 0, jsonBody: '' };
-
-  if (!id_product || Number.isNaN(id_product)) {
-    returnDTO.status = 400;
-    returnDTO.jsonBody = {
-      messages: [constantUtil.MsgInvalidID],
-    };
-    return returnDTO;
-  }
-
-  id_product = +id_product;
-
-  const product = jsonProducts.products.find(
-    (p) => p.id_product === id_product
-  );
-
-  if (!product) {
-    returnDTO.status = 404;
-    returnDTO.jsonBody = {
-      messages: [constantUtil.MsgStatus404],
-    };
-    return returnDTO;
-  }
-
-  returnDTO.status = 200;
-  returnDTO.jsonBody = product;
-
-  return returnDTO;
-}
-
 class ProductController {
-  findAll(req, res) {
-    return res.status(200).json(jsonProducts);
+  async findAll(req, res) {
+    const responseMethod = await productRepository.getAllProducts();
+
+    if (responseMethod.error) {
+      return res.status(500).json({ messages: [constantUtil.MsgStatus500] });
+    }
+
+    if (!responseMethod.response) {
+      return res.status(404).json({ messages: [constantUtil.MsgStatus404] });
+    }
+
+    const products = {
+      items: responseMethod.response.map((product) => product.toJSON()),
+    };
+
+    return res.status(200).json(products);
   }
 
-  findById(req, res) {
-    const { id } = req.params;
-    const returnDTO = findProductById(id);
-    return res.status(returnDTO.status).json(returnDTO.jsonBody);
+  async findById(req, res) {
+    let { id } = req.params;
+    id = +id;
+
+    if (!id || !Number.isSafeInteger(id)) {
+      return res.status(400).json({ messages: [constantUtil.MsgInvalidID] });
+    }
+
+    const responseMethod = await productRepository.getProductById(id);
+
+    if (responseMethod.error) {
+      return res.status(500).json({ messages: [constantUtil.MsgStatus500] });
+    }
+
+    if (!responseMethod.response) {
+      return res.status(404).json({ messages: [constantUtil.MsgStatus404] });
+    }
+
+    return res.status(200).json(responseMethod.response.toJSON());
   }
 }
 
