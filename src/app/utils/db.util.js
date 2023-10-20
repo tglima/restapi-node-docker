0f7e4a09503eace8319e2b5b52cedf31cf71a356
tglima <thg.limadesousa@gmail.com>
2023-10-20 18:45:10 -0300

    feat: Implementa a class DBUtil

---
+++ src/app/utils/db.util.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-console */
+const { Sequelize } = require('sequelize');
+const path = require('path');
+
+let instance;
+
+class DbUtil {
+  constructor() {
+    const dbPathSqLite = path.join(__dirname, '..', 'assets', 'database.db');
+    this.SQLite = new Sequelize({
+      dialect: 'sqlite',
+      storage: dbPathSqLite,
+      logging: false,
+    });
+  }
+
+  static getInstance() {
+    if (!instance) {
+      instance = new DbUtil();
+      Object.freeze(instance);
+    }
+    return instance;
+  }
+}
+
+export default DbUtil.getInstance();
