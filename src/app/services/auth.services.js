30edc478c84580d7c2a572798e09f093cd87343b
tglima <thg.limadesousa@gmail.com>
2023-10-28 08:33:34 -0300

    refact: Refatora o método checkAuth
    - Remove o método validateRequest da classe
    - Utiliza o método validateRequest da classe Validator
    - Alterado o método checkAuth, melhorando os logs

--- src/app/services/auth.services.js
+++ src/app/services/auth.services.js
@@ -1,86 +1,40 @@
 import logRepository, { TypesEvent } from '../repository/sqlite/log.repository';
-import constantUtil from '../utils/constant.util';
 import util from '../utils/util';
 import logService from './log.service';
-
-function validateRequest(req) {
-  const publicRoutes = ['/swagger', '/health-check/'];
-  const validKeys = process.env.API_KEY.split(';');
-  const returnMethod = {};
-  returnMethod.nm_method = 'validateRequest';
-  returnMethod.dt_start = util.getDateNow();
-  returnMethod.dt_finish = undefined;
-  returnMethod.was_error = false;
-  returnMethod.response = undefined;
-  const info = [];
-
-  info.push(`info: req.originalUrl = ${req.originalUrl}`);
-
-  let urlBase = req.originalUrl;
-
-  urlBase = urlBase.replace(`/api/v${process.env.NU_VERSION}`, '');
-  info.push(`info: urlBase = ${urlBase}`);
-
-  if (urlBase === '/' || publicRoutes.some((route) => urlBase.includes(route))) {
-    returnMethod.dt_finish = util.getDateNow();
-    returnMethod.response = true;
-    info.push("info: it's public route!");
-  }
-
-  const apiKey = !req.header('x-api-key') ? req.header('X-API-KEY') : req.header('x-api-key');
-
-  info.push(`info: apiKey from header = ${apiKey}`);
-
-  if (!returnMethod.dt_finish && apiKey && validKeys.includes(apiKey)) {
-    info.push("info: it's valid apiKey");
-    returnMethod.dt_finish = util.getDateNow();
-    returnMethod.response = true;
-  }
-
-  if (!returnMethod.dt_finish) {
-    info.push('info: invalid apiKey');
-    returnMethod.dt_finish = util.getDateNow();
-    returnMethod.response = false;
-  }
-
-  if (!returnMethod.dt_finish) {
-    info.push('info: method failure');
-    returnMethod.dt_finish = util.getDateNow();
-    returnMethod.response = false;
-  }
-
-  returnMethod.info = info;
-
-  if (!returnMethod.response) {
-    logService.info(JSON.stringify(returnMethod));
-  }
-  return returnMethod;
-}
+import validatorServices from './validator.services';
 
 class AuthService {
   async checkAuth(req, res, next) {
     const LogDTO = {
+      code_event: util.getNewCodeEvent(),
       dt_start: util.getDateNow(),
       dt_finish: undefined,
       type_event: TypesEvent.REQUEST,
       json_log_event: {
-        methods: undefined,
-        request_data: undefined,
-        response_data: undefined,
+        io_data: {
+          request_data: util.getRequestData(req),
+          response_data: undefined,
+        },
+        methods: [],
       },
     };
 
-    LogDTO.json_log_event.request_data = util.getRequestData(req);
-    const responseMethod = validateRequest(req);
+    const messages = [];
+    const respValidateRequest = validatorServices.validateRequest(req);
 
-    LogDTO.json_log_event.methods = [responseMethod];
+    LogDTO.json_log_event.methods.push(respValidateRequest);
 
-    if (!responseMethod.response) {
+    if (!respValidateRequest.response) {
+      respValidateRequest.messages.forEach((message) => {
+        messages.push(message);
+      });
+
       const responseAPI = {
         status: 401,
-        body: { messages: [constantUtil.MsgStatus401] },
+        body: { code_event: LogDTO.code_event, messages },
       };
 
+      LogDTO.json_log_event.io_data.request_data = util.getRequestData(req);
       LogDTO.json_log_event.response_data = responseAPI;
       logService.info('unauthorized request');
       logRepository.saveLogEvent(LogDTO);
