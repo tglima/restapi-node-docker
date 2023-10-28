2cfcb8643ebadeaf5dd130b14128bce334ecb759
tglima <thg.limadesousa@gmail.com>
2023-10-28 17:52:51 -0300

    feat: Implementa a classe ManagerRoutes Classe responsável pelos endpoints de gerenciamento da aplicação

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import healthRoutes from './health.routes';
+import managerRoutes from './manager.routes';
 import productRoutes from './product.routes';
 import swaggerRoutes from './swagger.routes';
 
@@ -15,7 +16,7 @@ class Routes {
     this.router.use('', swaggerRoutes);
     this.router.use('', healthRoutes);
     this.router.use(urlBase, productRoutes);
-
+    this.router.use(urlBase, managerRoutes);
     this.router.get('/', (req, res) => {
       res.redirect('/swagger');
     });
---
+++ src/app/routes/manager.routes.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import managerController from '../controllers/manager.controller';
+
+class ManagerRoutes {
+  constructor() {
+    this.router = Router();
+    this.router.get('/mng/log_events/find/', managerController.findLogEvent);
+  }
+
+  getRoutes() {
+    return this.router;
+  }
+}
+
+export default new ManagerRoutes().getRoutes();
