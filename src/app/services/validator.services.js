fe7dab6b763e7b7bbcf72446ee995eb67cb87bb5
tglima <thg.limadesousa@gmail.com>
2023-10-28 08:28:12 -0300

    feat: Implementa a class ValidatorService
    Classe responsável por centralizar as principais válidações da aplicação

---
+++ src/app/services/validator.services.js
@@ -0,0 +1,199 @@
+import moment from 'moment-timezone';
+import constantUtil from '../utils/constant.util';
+import util from '../utils/util';
+import logService from './log.service';
+
+let instance;
+
+class ValidatorService {
+  constructor() {
+    this.#DateTimeFormat = constantUtil.MomentDateFormat;
+  }
+
+  #DateTimeFormat;
+
+  validateDateTime(dateTime) {
+    const returnMethod = {
+      nm_method: 'validateDateTime',
+      dt_start: util.getDateNow(),
+      dt_finish: null,
+      was_error: null,
+      response: null,
+      info: [],
+      messages: [],
+    };
+
+    returnMethod.info.push(`info: dateTime = ${dateTime}`);
+    returnMethod.response = true;
+
+    if (!dateTime) {
+      returnMethod.response = false;
+      returnMethod.info.push(`info: ${constantUtil.MsgDateTimeIsEmpty}.`);
+      returnMethod.messages.push(constantUtil.MsgDateTimeIsEmpty);
+    } else if (!moment(dateTime, this.#DateTimeFormat, true).isValid()) {
+      returnMethod.response = false;
+      returnMethod.info.push(`info: ${constantUtil.MsgInvalidMomentDateFormat}.`);
+      returnMethod.messages.push(constantUtil.MsgInvalidMomentDateFormat);
+    }
+
+    returnMethod.dt_finish = util.getDateNow();
+    return returnMethod;
+  }
+
+  valDateTimeRange(startDate, finishDate) {
+    const returnMethod = {
+      nm_method: 'valDateTimeRange',
+      dt_start: util.getDateNow(),
+      dt_finish: null,
+      was_error: null,
+      response: null,
+      info: [],
+      message: '',
+    };
+    returnMethod.info.push(`info: startDate = ${startDate} finishDate = ${finishDate}`);
+
+    if (moment(startDate, this.#DateTimeFormat).isAfter(moment(finishDate, this.#DateTimeFormat))) {
+      returnMethod.message = constantUtil.MsgInvalidDateTimeRange;
+      returnMethod.response = false;
+    }
+
+    returnMethod.response =
+      returnMethod.response === null ? (returnMethod.response = true) : returnMethod.response;
+
+    if (!returnMethod.response) {
+      logService.info('invalid datetime range');
+    }
+
+    returnMethod.dt_finish = util.getDateNow();
+    return returnMethod;
+  }
+
+  validatePage(page) {
+    const returnMethod = {
+      nm_method: 'validatePage',
+      dt_start: util.getDateNow(),
+      dt_finish: null,
+      was_error: null,
+      response: null,
+      info: [],
+      messages: [],
+    };
+
+    returnMethod.response = true;
+    returnMethod.info.push(`info: page = ${page}`);
+
+    if (!page || !Number.isSafeInteger(page)) {
+      returnMethod.response = false;
+      returnMethod.messages.push(`page: ${constantUtil.MsgInvalidValue}`);
+    }
+
+    returnMethod.dt_finish = util.getDateNow();
+    return returnMethod;
+  }
+
+  validateCodeEvent(codeEvent) {
+    const returnMethod = {
+      nm_method: 'validateCodeEvent',
+      dt_start: util.getDateNow(),
+      dt_finish: null,
+      was_error: null,
+      response: null,
+      info: [],
+      messages: [],
+    };
+
+    returnMethod.response = true;
+    returnMethod.info.push(`info: codeEvent = ${codeEvent}`);
+
+    if (!constantUtil.CodeEventFormatRegex.test(codeEvent)) {
+      returnMethod.response = false;
+      returnMethod.messages.push(`codeEvent: ${constantUtil.MsgInvalidValue}`);
+    }
+
+    returnMethod.dt_finish = util.getDateNow();
+    return returnMethod;
+  }
+
+  valFormatApiKey(apiKey) {
+    const returnMethod = {
+      nm_method: 'valFormatApiKey',
+      dt_start: util.getDateNow(),
+      dt_finish: null,
+      was_error: null,
+      response: null,
+      info: [],
+      messages: [],
+    };
+
+    returnMethod.response = true;
+    returnMethod.info.push(`info: apiKey = ${apiKey}`);
+
+    if (!constantUtil.ApiKeyFormatRegex.test(apiKey)) {
+      returnMethod.response = false;
+      returnMethod.messages.push(`api_key: ${constantUtil.MsgInvalidValue}`);
+    }
+
+    returnMethod.dt_finish = util.getDateNow();
+    return returnMethod;
+  }
+
+  validateRequest(req) {
+    const returnMethod = {
+      nm_method: 'validateRequest',
+      dt_start: util.getDateNow(),
+      dt_finish: null,
+      was_error: null,
+      response: null,
+      info: [],
+      messages: [],
+    };
+
+    const publicRoutes = ['/swagger', '/health-check/'];
+    const validKeys = process.env.API_KEY.split(';');
+
+    returnMethod.info.push(`info: req.originalUrl = ${req.originalUrl}`);
+
+    let urlBase = req.originalUrl;
+
+    urlBase = urlBase.replace(`/api/v${process.env.NU_VERSION}`, '');
+    returnMethod.info.push(`info: urlBase = ${urlBase}`);
+
+    if (urlBase === '/' || publicRoutes.some((route) => urlBase.includes(route))) {
+      returnMethod.response = true;
+      returnMethod.info.push("info: it's public route!");
+    }
+
+    const apiKey = !req.header('api-key') ? req.header('API-KEY') : req.header('api-key');
+
+    returnMethod.info.push(`info: apiKey from header = ${apiKey}`);
+
+    if (returnMethod.response === null && apiKey && validKeys.includes(apiKey)) {
+      returnMethod.info.push("info: it's valid apiKey");
+      returnMethod.response = true;
+    }
+
+    if (returnMethod.response === null) {
+      returnMethod.info.push('info: invalid apiKey');
+      returnMethod.response = false;
+    }
+
+    if (!returnMethod.response) {
+      logService.info(JSON.stringify(returnMethod));
+      returnMethod.messages.push(`api-key: ${constantUtil.MsgStatus401}`);
+    }
+
+    returnMethod.dt_finish = util.getDateNow();
+
+    return returnMethod;
+  }
+
+  static getInstance() {
+    if (!instance) {
+      instance = new ValidatorService();
+      Object.freeze(instance);
+    }
+    return instance;
+  }
+}
+
+export default ValidatorService.getInstance();
