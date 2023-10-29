01928185736ddcf3beeff93ba18d1e98d9b10763
tglima <thg.limadesousa@gmail.com>
2023-10-29 16:46:27 -0300

    feat: Implementa novos métodos e propriedade - dbPathSQLite: Retorna o caminho completo do arquivo de banco de dados - getTableNames: Retorna o nome de todas as tabelas contidas no banco - getDataBaseSize: Retorna o tamanho em megabystes do arquivo do banco

--- src/app/utils/db.util.js
+++ src/app/utils/db.util.js
@@ -1,3 +1,8 @@
+import { stat } from 'fs/promises';
+import logService from '../services/log.service';
+import constantUtil from './constant.util';
+import util from './util';
+
 const { Sequelize } = require('sequelize');
 const path = require('path');
 
@@ -6,13 +11,70 @@ let instance;
 class DbUtil {
   constructor() {
     this.DataBaseFileName = process.env.DATABASE_FILE_NAME || 'database.db';
+    this.#dbPathSQLite = path.join(__dirname, '..', 'assets', this.DataBaseFileName);
 
-    const dbPathSqLite = path.join(__dirname, '..', 'assets', this.DataBaseFileName);
     this.SQLite = new Sequelize({
       dialect: 'sqlite',
-      storage: dbPathSqLite,
+      storage: this.#dbPathSQLite,
       logging: false,
     });
+  }
+
+  #dbPathSQLite;
+
+  async getTableNames() {
+    const returnMethod = {
+      nm_method: 'getTableNames',
+      dt_start: util.getDateNow(),
+      dt_finish: null,
+      was_error: null,
+      response: null,
+      info: [],
+      methods: [],
+    };
+
+    try {
+      const tableNames = await this.SQLite.getQueryInterface().showAllTables();
+      returnMethod.info.push(`info: tableNames = ${JSON.stringify(tableNames)}`);
+      returnMethod.response = tableNames;
+    } catch (error) {
+      returnMethod.info.push(`Error message: ${error.message}`);
+      returnMethod.messages.push(constantUtil.MsgErroDatabaseQuery);
+      logService.error(`method: ${returnMethod.nm_method} => Error: ${error}`);
+      returnMethod.was_error = true;
+      returnMethod.response = null;
+    }
+    returnMethod.dt_finish = util.getDateNow();
+    return returnMethod;
+  }
+
+  async getDataBaseSize() {
+    const returnMethod = {
+      nm_method: 'getDataBaseSize',
+      dt_start: util.getDateNow(),
+      dt_finish: null,
+      was_error: null,
+      response: null,
+      info: [],
+      methods: [],
+    };
+
+    try {
+      const stats = await stat(this.#dbPathSQLite);
+      const fileSizeInBytes = stats.size;
+      const fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024); // Tamanho em megabytes
+      returnMethod.info.push(`info: fileSizeInMegabytes = ${fileSizeInMegabytes}`);
+      returnMethod.response = fileSizeInMegabytes;
+    } catch (error) {
+      returnMethod.info.push(`Error message: ${error.message}`);
+      returnMethod.messages.push(constantUtil.MsgErrorGetInfoFile);
+      logService.error(`method: ${returnMethod.nm_method} => Error: ${error}`);
+      returnMethod.was_error = true;
+      returnMethod.response = null;
+    }
+
+    returnMethod.dt_finish = util.getDateNow();
+    return returnMethod;
   }
 
   static getInstance() {
