182e7e47f5fc99fde7f526fb3b3b3ec22f0e50f1
tglima <thg.limadesousa@gmail.com>
2023-10-30 19:37:03 -0300

    feat: Adiciona a rota /mng/database/delete

--- src/app/routes/manager.routes.js
+++ src/app/routes/manager.routes.js
@@ -7,6 +7,7 @@ class ManagerRoutes {
     this.router.get('/mng/database-backup/', managerController.getDataBaseFile);
     this.router.get('/mng/database-info/', managerController.getDataBaseInfo);
     this.router.get('/mng/log-events/find/', managerController.findLogEvent);
+    this.router.delete('/mng/database/delete', managerController.delRowsDatabase);
   }
 
   getRoutes() {
