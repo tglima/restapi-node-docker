76e8f8985fef4878221cecef8a300d8e26388e91
tglima <thg.limadesousa@gmail.com>
2023-11-01 22:16:27 -0300

    feat: Implementa novos métodos getMngSwaggerJSON setupMngSwaggerUIMng getMngSwaggerDocument

--- src/app/controllers/swagger.controller.js
+++ src/app/controllers/swagger.controller.js
@@ -1,6 +1,8 @@
 import swaggerUi from 'swagger-ui-express';
+import constantUtil from '../utils/constant.util';
 
 const swaggerJson = require('../assets/swagger.json');
+const swaggerManagerJson = require('../assets/swagger-manager.json');
 
 const customOptions = {
   customCss: process.env.SWAGGER_CUSTOM_CSS,
@@ -47,6 +49,22 @@ function getSwaggerDocument() {
   return swaggerDocument;
 }
 
+function getMngSwaggerDocument() {
+  const QtLimitDelete = process.env.QT_LIMIT_DELETE || '10';
+  const MsgDatabaseDeleteRows = constantUtil.MsgDatabaseDeleteRows.replace('{{VALUE}}', QtLimitDelete);
+
+  let document = swaggerManagerJson;
+
+  document = JSON.stringify(document);
+  document = document.replace('{{NU_VERSION}}', process.env.NU_VERSION);
+  document = document.replace('{{QTD_ITEMS_DELETE}}', QtLimitDelete);
+  document = document.replace('{{MSG_DATABASE_DELETE_ROWS}}', MsgDatabaseDeleteRows);
+  document = document.replace('{{SWAGGER_SERVERS_DESCRIPTION}}', process.env.SWAGGER_SERVERS_DESCRIPTION);
+  document = document.replace('{{SWAGGER_INFO_DESCRIPTION_MNG}}', process.env.SWAGGER_INFO_DESCRIPTION_MNG);
+
+  return JSON.parse(document);
+}
+
 class SwaggerController {
   getSwaggerJSON(req, res) {
     res.json(getSwaggerDocument());
@@ -54,6 +72,14 @@ class SwaggerController {
 
   setupSwaggerUI() {
     return swaggerUi.setup(getSwaggerDocument(), customOptions);
+  }
+
+  getMngSwaggerJSON(req, res) {
+    res.json(getMngSwaggerDocument());
+  }
+
+  setupMngSwaggerUIMng() {
+    return swaggerUi.setup(getMngSwaggerDocument(), customOptions);
   }
 }
 
