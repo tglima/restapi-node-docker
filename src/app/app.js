c471bd691924f784251fba3f9936a2e90c9f9bb3
tglima <thg.limadesousa@gmail.com>
2023-11-03 08:16:54 -0300

    refact: Altera como são utilizadas as rotas - index.js: Removida a classe - index.routes.js: Substituir a index.js - app.js: Simplificação do código e utilização do express-async-errors

--- src/app/app.js
+++ src/app/app.js
@@ -1,11 +1,11 @@
 import express from 'express';
+import 'express-async-errors';
 import helmet from 'helmet';
 import morganBody from 'morgan-body';
 import Youch from 'youch';
-import routes from './routes';
+import Routes from './routes/index.routes';
 import authServices from './services/auth.services';
 import logService from './services/log.service';
-import util from './utils';
 import constantUtil from './utils/constant.util';
 import dbUtil from './utils/db.util';
 
@@ -13,10 +13,12 @@ class App {
   constructor() {
     this.server = express();
     this.#port = process.env.NU_PORT;
+    this.#routes = Routes;
+    this.#routes.setupRoutes(this.server);
     this.#middlewares();
-    this.#loadRoutes();
-    this.#exceptionHandler();
   }
+
+  #routes;
 
   #port;
 
@@ -38,19 +40,11 @@ class App {
   #middlewares() {
     this.server.use(express.json());
     this.server.use(helmet());
-    this.server.use('/', util.rateLimitAPI());
-    this.server.use(`/v${process.env.NU_VERSION}/mng/database-backup/`, util.rateLimitAPI(1));
     this.server.use(authServices.checkAuth);
     if (process.env.MUST_RUN_MORGAN_BODY) {
       morganBody(this.server);
     }
-  }
-
-  #loadRoutes() {
-    this.server.use(routes);
-    this.server.use((req, res, next) => {
-      res.status(404).send();
-    });
+    this.#exceptionHandler();
   }
 
   #exceptionHandler() {
---
+++ src/app/routes/index.routes.js
@@ -0,0 +1,65 @@
+import swaggerUi from 'swagger-ui-express';
+import managerController from '../controllers/manager.controller';
+import productController from '../controllers/product.controller';
+import swaggerController from '../controllers/swagger.controller';
+import utils from '../utils';
+
+const urlBase = `/v${process.env.NU_VERSION}`;
+const mngDBBackup = `${urlBase}/mng/database-backup/`;
+const mngDBinfo = `${urlBase}/mng/database-info/`;
+const mngDbDelete = `${urlBase}/mng/database/delete`;
+const mngLogEventsFind = `${urlBase}/mng/log-events/find/`;
+const productFind = `${urlBase}/products/find`;
+const swaggerJSON = '/swagger.json';
+const swaggerManagerJSON = '/swagger-manager.json';
+const swagger = '/swagger';
+const swaggerManager = '/swagger-manager';
+const healthCheck = '/health-check';
+
+class Routes {
+  constructor() {
+    this.#SwaggerJSON = swaggerController.getSwaggerJSON();
+    this.#MngSwaggerJSON = swaggerController.getMngSwaggerJSON();
+  }
+
+  #SwaggerJSON;
+
+  #MngSwaggerJSON;
+
+  setupRoutes(router) {
+    router.use('/', utils.rateLimitAPI());
+    router.get('/', (req, res) => {
+      res.redirect(swagger);
+    });
+
+    router.get(healthCheck, (req, res) => {
+      res.status(200).json({
+        status: 'OK',
+        message: 'API IS UP!',
+      });
+    });
+
+    router.get(mngDBinfo, managerController.getDataBaseInfo);
+    router.use(mngDBBackup, utils.rateLimitAPI(1));
+    router.get(mngDBBackup, managerController.getDataBaseFile);
+    router.get(mngLogEventsFind, managerController.findLogEvent);
+    router.delete(mngDbDelete, managerController.delRowsDatabase);
+
+    router.get(productFind, productController.find);
+
+    router.get(swaggerJSON, swaggerController.respSwaggerJSON);
+    router.get(swaggerManagerJSON, swaggerController.respSwaggerJSON);
+    router.use(
+      swagger,
+      swaggerUi.serveFiles(this.#SwaggerJSON),
+      swaggerController.setupSwaggerUI(this.#SwaggerJSON)
+    );
+    router.use(
+      swaggerManager,
+      swaggerUi.serveFiles(this.#MngSwaggerJSON, swaggerController.getCustomOptions),
+      swaggerController.setupSwaggerUI(this.#MngSwaggerJSON, swaggerController.getCustomOptions)
+    );
+  }
+}
+
+export default new Routes();
