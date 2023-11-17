348ff8ff5abc366548b22378163a7d0c8fb857fd
tglima <thg.limadesousa@gmail.com>
2023-11-17 20:19:38 -0300

    refact: Altera para excluir os zips gerados

--- src/__test__/globalTeardown.js
+++ src/__test__/globalTeardown.js
@@ -1,17 +1,21 @@
 /* eslint-disable no-console */
 const fs = require('fs').promises;
+const { default: utils } = require('../app/utils');
 const { targetTestDbPath, testDbFileName } = require('./testUtil');
 
 module.exports = async () => {
   // Faz isso para encerrar o express no final dos testes
   await global.server.close();
 
+  // Apaga os arquivos zips que foram criados durante os testes
+  await utils.deleteOldZip();
+
   try {
-    // Aguarda 3 segundos para remover o db de testes
+    // Aguarda 2 segundos para remover o db de testes
     setTimeout(() => {
       fs.unlink(targetTestDbPath);
       console.log(`file ${testDbFileName} has been deleted`);
-    }, 3000);
+    }, 2000);
   } catch (err) {
     console.error(err);
   }
