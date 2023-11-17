890d38cfd987266303cc4fc3ca6665d50a44f22b
tglima <thg.limadesousa@gmail.com>
2023-11-17 19:03:37 -0300

    feat: Implementa a classe Request - Classe criada apenas para se parecer com o objeto req utilizado pelas routes e controllers.

--- src/__test__/testUtil.js
+++ src/__test__/testUtil.js
@@ -15,6 +15,30 @@ const urlLogEvents = `/v${constantUtil.NuVersionAPI}/mng/log-events`;
 const urlProductFind = `/v${constantUtil.NuVersionAPI}/products/find`;
 const momentDateFormat = constantUtil.MomentDateFormat;
 
+/**
+ * Request
+ *
+ * Classe criada para reproduzir o objeto req. Seu objetivo é
+ * ser apenas como um tipo de mock para facilitar durante os
+ * testes. Não utilizar esse classe fora do ambiente de testes
+ *
+ * @version 1.0
+ */
+class Request {
+  constructor() {
+    this.headers = {};
+    this.originalUrl = `/api/v${constantUtil.NuVersionAPI}/mng/log-events/find`;
+  }
+
+  headers;
+
+  originalUrl;
+
+  header(key) {
+    return this.headers[key] ?? null;
+  }
+}
+
 module.exports = {
   apiKey,
   authorization,
@@ -28,4 +52,5 @@ module.exports = {
   sourceTestDbPath,
   targetTestDbPath,
   testDbFileName,
+  Request,
 };
